97cddd7b1bd702b860b17dcfbde297e645a6145a
TimeoBINDA <122312331+TimeoBINDA@users.noreply.github.com>
2023-05-16 15:45:06 +0200

    Début_text_defilant

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import FooterPage from './components/FooterPage.vue';
 import googleMaps from './components/googleMaps.vue';
 import HeaderPage from './components/HeaderPage.vue';
+import TexteDefilant from './components/TexteDefilant.vue';
 </script>
 
 <template>
+  <TexteDefilant/>
   <HeaderPage/>
   <googleMaps/>
   <main>Le contenu principal</main>
---
+++ src/components/TexteDefilant.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <vue-ticker :items="items" />
+    </div>
+</template>
+
+<script>
+import VueTicker from 'vue-ticker'
+
+export default {
+    components: {
+        VueTicker
+    },
+    data() {
+        return {
+            items: [
+                'Texte défilant 1',
+                'Texte défilant 2',
+                'Texte défilant 3'
+            ]
+        }
+    }
+}
+</script>
