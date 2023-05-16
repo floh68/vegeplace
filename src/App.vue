5c29b659e5c94b15e0bfcd40e6fd61752ebbe29a
TimeoBINDA <122312331+TimeoBINDA@users.noreply.github.com>
2023-05-16 09:43:14 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import FooterPage from './components/FooterPage.vue';
 import googleMaps from './components/googleMaps.vue';
+import HeaderPage from './components/HeaderPage.vue';
 </script>
 
 <template>
+  <HeaderPage/>
   <googleMaps/>
   <main>Le contenu principal</main>
   <FooterPage/>
