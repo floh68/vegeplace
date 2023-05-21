24ed46ec465c8d478691d1e196f87fcb6cb42bd3
Florian Houtmann <105522552+floh68@users.noreply.github.com>
2023-05-21 14:15:26 +0200

    google maps

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 //import textedefilant from './components/textedefilant.vue';
 import FooterPage from './components/FooterPage.vue';
-//import googleMaps from './components/googleMaps.vue';
+import googleMaps from './components/googleMaps.vue';
 import HeaderPage from './components/HeaderPage.vue';
 //import TexteDefilant from './components/TexteDefilant.vue';
 import { RouterView } from 'vue-router';
--- src/components/googleMaps.vue
+++ src/components/googleMaps.vue
@@ -1,41 +1,16 @@
-<!-- <template>
-  <div id="map"></div>
-</template>
-
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-// Définir votre clé API ici également
-const apiKey = 'VOTRE_CLE_API';
-
-// Définir les coordonnées du centre de la carte
-const center = { lat: 37.7749, lng: -122.4194 };
-
-// Définir la fonction d'initialisation de la carte
-const initMap = () => {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center,
-    zoom: 12,
-  });
-
-  // Ajouter un marqueur sur la carte
-  const marker = new google.maps.Marker({
-    position: center,
-    map,
-    title: 'Hello World!',
-  });
-};
-
-// Créer une référence pour stocker la carte
-const mapRef = ref<google.maps.Map | null>(null);
-
-// Effectuer l'initialisation de la carte lorsque le composant est monté
-onMounted(() => {
-  // Charger l'API Google Maps avec la clé API
-  const script = document.createElement('script');
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
-  script.onload = initMap;
-  document.head.appendChild(script);
-});
-</script>
- -->
+<template>
+  <GoogleMap
+  api-key="AIzaSyChXK5Iz8zTGhMk3hr0-BTzNjiLb42Snwo"
+  style="width: 100%; height: 500px"
+  :center="center"
+  :zoom="15"
+  >
+  <Marker :options="{ position: center }" />
+  </GoogleMap>
+ </template>
+ 
+ <script setup lang="ts">
+ import { GoogleMap, Marker } from 'vue3-google-map'
+ 
+ const center = { lat: 47.49566525144965, lng: 6.805222081286718 }
+ </script>
